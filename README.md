8704d0395053dee27f9e8f540452e8a47dda5c63
Guangxuan Li <ligx98@gmail.com>
2021-10-04 17:19:26 -0400

    implemented top level

--- README.md
+++ README.md
@@ -19,10 +19,10 @@ regfile
 └───tristateBuffer
 ```
 
-- [ ] regfile.v: top level entity, register file with 1 write port and 2 read ports.
+- [x] regfile.v: top level entity, register file with 1 write port and 2 read ports.
 - [x] dffe_WeRs.v: implements D-Flip-Flop with Write-Enable and asynchronous Reset.
-- [ ] decoder.v: just a decoder.
-- [ ] and_arr.v: an and gate that takes an array and AND all the bits together.
+- [x] decoder.v: just a decoder.
+- [x] and_arr.v: an and gate that takes an array and AND all the bits together.
 - [x] tristateBuffer.v: a tri-state buffer that outputs high-impedance when Enable is 0.
 
 ## 🐞 Bugs and Issues
--- regfile.v
+++ regfile.v
@@ -15,5 +15,65 @@ module regfile (clock,
     output [31:0] data_readRegA, data_readRegB;
     
     /* YOUR CODE HERE */
-    
+
+    // delclare wires
+    wire [31:0] dec_write, dec_readA, dec_readB;
+    wire [31:1] reg_en;
+    wire [31:0] reg_out [31:0];
+
+    // write port
+    decoder decoder_write(
+        .sel(ctrl_writeReg),
+        .out(dec_write)
+    );
+    genvar i;
+    generate
+        for (i = 1; i < 32; i = i + 1) begin: gen_and
+            and u_and(reg_en[i], ctrl_writeEnable, dec_write[i]);
+        end
+    endgenerate
+
+    // registers
+    assign reg_out[0] = 32'h00000000;
+    generate
+        for (i = 1; i < 32; i = i + 1) begin: gen_reg
+            dffe_WeRs u_dffe_WeRs(
+                .we(reg_en[i]),
+                .din(data_writeReg),
+                .qout(reg_out[i]),
+                .clk(clock),
+                .rst(ctrl_reset)
+            );
+        end
+    endgenerate
+
+    // read port A
+    decoder decoder_readA(
+        .sel(ctrl_readRegA),
+        .out(dec_readA)
+    );
+    generate
+        for (i = 0; i < 32; i = i + 1) begin: gen_readA
+            tristateBuffer u_tristateBuffer(
+                .Din(reg_out[i]),
+                .Dout(data_readRegA),
+                .en(dec_readA[i])
+            );
+        end
+    endgenerate
+
+    // read port B
+    decoder decoder_readB(
+        .sel(ctrl_readRegB),
+        .out(dec_readB)
+    );
+    generate
+        for (i = 0; i < 32; i = i + 1) begin: gen_readB
+            tristateBuffer u_tristateBuffer(
+                .Din(reg_out[i]),
+                .Dout(data_readRegB),
+                .en(dec_readB[i])
+            );
+        end
+    endgenerate
 endmodule
